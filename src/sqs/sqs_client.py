1a906d6e6cf7c9633ca5d30ffd1eb897d552f15b
Brian Rivera <bri_riva@hotmail.com>
2023-07-23 00:48:19 -0600

    [Updated] Cambio de message attribute

--- src/sqs/sqs_client.py
+++ src/sqs/sqs_client.py
@@ -7,18 +7,11 @@ from src.config.enviroments import ENVS
 
 class SQSClient:
     def __init__(self):
-        self.data = ""
+        pass
 
     @staticmethod
     def send_message(message):
         sqs_client = boto3.client('sqs')
-        message_attributes = {
-            'MessageType': {
-                'DataType': 'String',
-                'StringValue': 'JSON'
-            }
-        }
 
         return sqs_client.send_message(QueueUrl=ENVS.SQS_URL,
-                                       MessageBody=json.dumps(message),
-                                       MessageAttributes=message_attributes)
+                                       MessageBody=json.dumps(message))
