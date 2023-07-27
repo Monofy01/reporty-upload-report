b4f1f76ccc685f86c5924b09268adf4a6669e57b
Brian Rivera <bri_riva@hotmail.com>
2023-07-27 02:43:14 -0600

    [Fixed] Se anexa metodo para poder serialziar excepcion v4

--- app.py
+++ app.py
@@ -9,59 +9,56 @@ from src.utils.constants.constants import Http
 
 def handler(event, context):
     try:
-            # SECCION DE DECODING
-            print(f"REQUEST :: {event}")
-            body = json.loads(event['body'])
-            excel_data = body['excel']
-            email_data = body['email']
+        # SECCION DE DECODING
+        print(f"REQUEST :: {event}")
+        body = json.loads(event['body'])
+        excel_data = body['excel']
+        email_data = body['email']
 
-            log_output = list()
+        log_output = list()
 
-            # SECCION DE PROCESAMIENTO
-            excel_object = Excel(
-                filename=excel_data['filename'],
-                webhook=excel_data['webhook'],
-                sheets=[Sheet.from_dict(item) for item in excel_data['sheets']]
-            )
+        # SECCION DE PROCESAMIENTO
+        excel_object = Excel(
+            filename=excel_data['filename'],
+            webhook=excel_data['webhook'],
+            sheets=[Sheet.from_dict(item) for item in excel_data['sheets']]
+        )
 
-            # ACUMULACION DE ERRORES
-            log_output.extend(excel_object.log_output)
-            for s in excel_object.sheets:
-                if len(s.log_output) > 0:
-                    log_output.append(s.log_output)
+        # ACUMULACION DE ERRORES
+        log_output.extend(excel_object.log_output)
+        for s in excel_object.sheets:
+            if len(s.log_output) > 0:
+                log_output.append(s.log_output)
 
-            if len(log_output) > 0:
-                # print(F"ERRORES FINALES {log_output}")
-                return {
-                    'statusCode': Http.UNPROCESSABLE,
-                    'body': log_output,
-                    'headers': {
-                        'Content-Type': 'application/json'
-                    }
+        if len(log_output) > 0:
+            return {
+                'statusCode': Http.UNPROCESSABLE,
+                'body': log_output,
+                'headers': {
+                    'Content-Type': 'application/json'
                 }
-            else:
-                # SECCION DE PROCESAMIENTO
-                try:
-                    ReportService.upload_report(excel_object, email_data)
-                    return {
-                        'statusCode': Http.SUCCESS,
-                        'body': json.dumps({
-                            'message': f'Se ha enviado exitosamente el reporte con el nombre {excel_object.filename}',
-                            'code': Http.SUCCESS,
-                        }),
-                        'headers': {
-                            'Content-Type': 'application/json'
-                        }
-                    }
-                except Exception as e:
-                    log_output.append(ReporteExistente().to_dict())
-                    return {
-                        'statusCode': Http.UNPROCESSABLE,
-                        'body': log_output,
-                        'headers': {
-                            'Content-Type': 'application/json'
-                        }
-                    }
+            }
+        else:
+            ReportService.upload_report(excel_object, email_data)
+            return {
+                'statusCode': Http.SUCCESS,
+                'body': json.dumps({
+                    'message': f'Se ha enviado exitosamente el reporte con el nombre {excel_object.filename}',
+                    'code': Http.SUCCESS,
+                }),
+                'headers': {
+                    'Content-Type': 'application/json'
+                }
+            }
+    except ReporteExistente as e:
+        log_output = [e.to_dict()]
+        return {
+            'statusCode': Http.UNPROCESSABLE,
+            'body': log_output,
+            'headers': {
+                'Content-Type': 'application/json'
+            }
+        }
     except Exception as e:
         print(f"ERROR :: Ha ocurrido un error generico :: {e}")
         return {
